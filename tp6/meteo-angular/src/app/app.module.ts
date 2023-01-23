a68940d282f18bcd4a90b8cfe7bacd725a9737dd
SAIDIDJ-Nadia <115471379+SAIDIDJ-Nadia@users.noreply.github.com>
2023-01-23 16:25:14 +0100

    tp6

--- tp6/meteo-angular/src/app/app.module.ts
+++ tp6/meteo-angular/src/app/app.module.ts
@@ -3,16 +3,43 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { MeteoComponent } from './meteo/meteo.component';
+const appRoutes: Routes = [
+  {
+    path: '', // la page principale utilisera le component suivant
+    component: MeteoComponent,
+  },
+  {
+    path: 'meteo/:name', // la page affichant la météo prendra comme paramètre 'name'
+    component: MeteoDetailComponent, // Ce component fera l'appel AJAX et afficher les données reçues par openWeatherMap
+  },
+  {
+    path: '**', // un chemin vers une page inexistante redirigera vers '/'
+    redirectTo: '/',
+    pathMatch: 'full',
+  },
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeteoComponent,
+    MeteoDetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+
   ],
   providers: [],
   bootstrap: [AppComponent]
+
 })
 export class AppModule { }
+import { RouterModule, Routes } from '@angular/router';
+import { MeteoDetailComponent } from './meteo-detail/meteo-detail.component';
+import { FormsModule } from '@angular/forms';
