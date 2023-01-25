accbac7ef233ed46a26fa7c66f5b1e82692001e9
SAIDIDJ-Nadia <115471379+SAIDIDJ-Nadia@users.noreply.github.com>
2023-01-25 13:25:58 +0100

    tp6

--- tp6/meteo-angular/src/app/app.module.ts
+++ tp6/meteo-angular/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
+import { MeteoDetailComponent } from './meteo-detail/meteo-detail.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,6 +43,3 @@ const appRoutes: Routes = [
 
 })
 export class AppModule { }
-import { RouterModule, Routes } from '@angular/router';
-import { MeteoDetailComponent } from './meteo-detail/meteo-detail.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
